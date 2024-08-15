b4b110745342e827828aa97ae385dae5dcdecbed
shuva9800 <ffshuva@gmail.com>
2024-08-15 12:53:31 +0530

    add netlify frontent url in cors

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin: 'http://localhost:5173', 
+    origin: 'https://master--mernstack-realestate.netlify.app', 
     credentials: true 
 }));
 
